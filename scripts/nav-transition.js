77c6984573a9090f8593f9c81d0fc420ccccca38
AlexVipond <alex.r.vipond@gmail.com>
2017-12-26 15:12:08 -0600

    Layout and style changes
    - Repair nav-transition.js
    - Delete unneeded main pages
    - Wrap email form in <form> element for accessibility

--- scripts/nav-transition.js
+++ scripts/nav-transition.js
@@ -1,11 +1,10 @@
-let page = document.querySelector('html'),
-    nav = document.querySelector('nav'),
+let nav = document.querySelector('nav'),
     header = document.querySelector('header');
 
 let scrollTop, navHeight, headerHeight, breakPoint;
 
 window.addEventListener('scroll', function() {
-    scrollTop = page.scrollTop;
+    scrollTop = window.pageYOffset;
     navHeight = nav.offsetHeight;
     headerHeight = header.offsetHeight;
     breakPoint = headerHeight * .7;
@@ -14,19 +13,19 @@ window.addEventListener('scroll', function() {
         menu.classList.remove('open-menu'); // menu is declared by open-menu.js
         menuIcon.classList.remove('open-menu'); // menuIcon is declared by open-menu.js
     } else if(scrollTop < navHeight) {
-        nav.style.position = 'relative';
+        nav.style.position = 'absolute';
         nav.style.top = '0';
         nav.style.opacity = 1;
         nav.classList.remove('fixed');
     } else if(scrollTop > navHeight && scrollTop < breakPoint) {
-        nav.style.position = 'relative';
+        nav.style.position = 'absolute';
         nav.style.top = headerHeight.toString() + 'px';
         nav.style.opacity = '0';
         nav.classList.add('fixed');
         menu.classList.remove('open-menu');
         menuIcon.classList.remove('open-menu');
     } else if(scrollTop > breakPoint && scrollTop < headerHeight) {
-        nav.style.position = 'relative';
+        nav.style.position = 'absolute';
         nav.style.top = headerHeight.toString() + 'px';
         nav.style.opacity = ((scrollTop - breakPoint) / (headerHeight - breakPoint)).toString();
         nav.classList.add('fixed');
